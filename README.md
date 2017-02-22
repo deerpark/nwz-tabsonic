56e8141aac9280dadca3c3685d802d6f221252c4
gms-ji9 <ji9@neowiz.com>
2017-02-22 11:17:23 +0900

    README.md update

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# nwz-tabsonic
+# Tabsonic for HTML5 prototype
+PIXI.js를 기반으로 자동화빌드 도구(Grunt)가 사용되었습니다.<br>
+bower.json 및 package.json 참고 하시면 종속성이 있는 플러그인들이 사용 되었음을 알 수 있습니다.
+
+## Setting environment
+- [Git](https://git-scm.com/) 버전관리 시스템
+- [Node.js](https://nodejs.org/ko/) 패키지 관리 도구
+	- Node.js는 필수 설치가 되어있어야 합니다.
+- [bower](https://bower.io/) 웹 패키지 관리 도구 (install 참고)
+- [grunt](http://gruntjs.com/) 빌드 자동화 도구 (install 참고)
+
+## Install
+Node.js / GIT 설치 후 GIT bash(터미널)에서 작업 폴더로 이동 후 아래 명령어 실행
+
+    $ mk dir workFolderName && cd workFolderName
+    $ npm init
+	$ git init
+    $ git clone git@github.com:deerpark/nwz-tabsonic.git
+    $ npm install //package.json 종속성이 있는 플러그인 설치
+    $ bower install //bower.json 종속성이 있는 플러그인 설치
+
+## Serve / Build
+작업 폴더에서 아래 명령어로 빌드 또는 로컬서버 로 테스트
+
+	$ grunt serve //로컬 서버 실행 및 테스트
+    $ grunt build //최종 빌드(빌드가 되었을 경우 $ git tag로 버전 명시)
+
+## PIXI.js
+http://www.pixijs.com/<br>
+무료 게임엔진 프레임워크로써 게임 API를 지원합니다.<br>
+- [공식 API](http://pixijs.download/release/docs/index.html)
+- [예제](http://www.pixijs.com/gallery)
+
+## Agile software development
+2주 간격으로 스프린트 목표를 설정 후 진행하고 리뷰합니다.
+상세한 개발방법이 적용되진 않았으나 현재까진 2주 스프린트로 진행 되었습니다.
+
+## Version
+버전의 규칙은 떠로 정해지진 않았지만<br>
+v(주 빌드).(커밋).(핫픽스) 형식으로<br>
+현재 v1.0.7 을 유지하고 있습니다.
+
+## File architecture
+- **app/** (실질적 작업 폴더)
+ 	- **images/**
+ 	 	- **game/**
+ 	- **scripts/**
+ 	 	- **controller/** (컨트롤에 관련된 js 모듈 집합 폴더)
+ 	 	 	- *game-loop-controller.js* (animation loop 관련 모듈)
+ 	 	 	- *scene-controller.js* (Scene 이동관련 모듈)
+ 	 	- **scene/**
+ 	 	 	- *game-scene.js* (메인 Scene)
+ 	 	 	- *main-menu-scene.js* (메인 메뉴)
+ 	 	- *intro.js* (로딩 및 초기화 모듈)
+ 	 	- *outro.js* (게임 종료 관련)
+ 	- **styles/**
+ 	 	- *main.css*
+- *index.html* (자동으로 빌드 되는 index.html)
+- **bower-components/** (bower 패키지 폴더)
+- **dist/** (빌드 최종 폴더)
+- **node-modules/** (npm 패키지 폴더)
+- *bower.json* (bower 설정 및 패키지 리스트)
+- *Gruntfile.js* (grunt 빌드 관련 설정)
+- *package.json* (npm 패키지 설정 및 리스트)
+- *README.md* (현재 보고있는 문서 md파일)
+- *template.index.html* (wiredep을 활용해 index.html을 자동으로 생성해주기 위한 템플릿)
--- app/scripts/outro.js
+++ app/scripts/outro.js
@@ -1,202 +1,25 @@
-/*
-var app = new PIXI.Application(450, 600, { antialias: true });
-window.addEventListener("DOMContentLoaded", function(){
-	document.body.appendChild(app.view);
-}, false);
-
-var GAME = GAME || {};
-
-GAME.BackgroundElement = function(texture, y, owner)
-{
-  this.sprites = [];
-  this.spriteWidth = texture.width-1;
-  var amount = Math.ceil(940 / this.spriteWidth);
-  if(amount < 3)amount = 3;
-  
-  for (var i=0; i < amount; i++) 
-  {
-    var sprite = new PIXI.Sprite(texture);
-    sprite.position.y = y;
-    owner.addChild(sprite);
-    this.sprites.push(sprite);
-  };  
-          
-  this.speed = 1;
+function isIntersecting(r1, r2) {
+    return !(r2.x > (r1.x + r1.width) ||
+        (r2.x + r2.width) < r1.x ||
+        r2.y > (r1.y + r1.height) ||
+        (r2.y + r2.height) < r1.y);
 }
 
-var
-rects = new PIXI.Container();
-rect1 = new PIXI.Graphics(),
-rect2 = new PIXI.Graphics(),
-rect3 = new PIXI.Graphics(),
-rect4 = new PIXI.Graphics(),
-line = new PIXI.Graphics(),
-style = new PIXI.TextStyle({
-    fontSize: 36,
-    fontWeight: 'bold',
-    fill: ['#ffffff', '#00ff99'], // gradient
-    stroke: '#4a1850',
-    strokeThickness: 5,
-    dropShadow: true,
-    dropShadowColor: '#000000',
-    dropShadowBlur: 4,
-    dropShadowAngle: Math.PI / 6,
-    dropShadowDistance: 6,
-    wordWrap: true,
-    wordWrapWidth: 440
-}),
-style2 = new PIXI.TextStyle({
-    fontSize: 20,
-    fontWeight: 'bold',
-    fill: ['white', 'yellow'], // gradient
-    stroke: 'red',
-    strokeThickness: 2,
-    dropShadow: true,
-    dropShadowColor: '#000000',
-    dropShadowBlur: 3,
-    dropShadowAngle: Math.PI / 2,
-    dropShadowDistance: 3
-}),
-count = 0,
-startText = new PIXI.Text('Start', style),
-countText = new PIXI.Text("0", style2);
+function shuffleRandom(n) {
+    var ar = new Array();
+    var temp;
+    var rnum;
 
-startText.x = app.renderer.width/2 - startText.width/2;
-startText.y = app.renderer.height/2 - startText.height/2;
-startText.interactive = true;
-startText.buttonMode = true;
-startText.on('pointerdown', onClick);
-
-countText.x = 400;
-countText.y = 540;
-
-app.stage.addChild(startText);
-app.stage.addChild(countText);
-
-// draw a rounded rectangle
-rect1.beginFill(0xFFFFFF, 1);
-rect1.drawRoundedRect(10, -10, 100, 10, 3);
-rect1.endFill();
-rect1.vx = 1;
-rect1.vy = 2;
-
-// draw a rounded rectangle
-rect2.beginFill(0xFFFFFF, 1);
-rect2.drawRoundedRect(120, -10, 100, 10, 3);
-rect2.endFill();
-rect2.vx = 1;
-rect2.vy = 10;
-
-// draw a rounded rectangle
-rect3.beginFill(0xFFFFFF, 1);
-rect3.drawRoundedRect(230, -10, 100, 10, 3);
-rect3.endFill();
-rect3.vx = 1;
-rect3.vy = 3;
-
-// draw a rounded rectangle
-rect4.beginFill(0xFFFFFF, 1);
-rect4.drawRoundedRect(340, -10, 100, 10, 3);
-rect4.endFill();
-rect4.vx = 1;
-rect4.vy = 5;
-
-// draw a rounded rectangle
-line.lineStyle(2, 0xFF3300, 1);
-line.moveTo(0, 0);
-line.lineTo(450, 0);
-line.x = 0;
-line.y = 500;
-app.stage.addChild(line);
-
-rects.addChild(rect1);
-rects.addChild(rect2);
-rects.addChild(rect3);
-rects.addChild(rect4);
-app.stage.addChild(rects);
-
-function onClick () {
-	startText.visible = false;
-	drop.start();
-}
-
-var drop = PIXI.ticker.shared;
-drop.autoStart = false;
-drop.add(function(delta){
-	rect1.y += rect1.vy;
-	rect2.y += rect2.vy;
-	rect3.y += rect3.vy;
-	rect4.y += rect4.vy;
-	if(rect1.y>500) {
-		rect1.y = -10;
-		count++;
-		countText.text = count;
-	}
-	if(rect2.y>500) {
-		rect2.y = -10;
-		count++;
-		countText.text = count;
-	}
-	if(rect3.y>500) {
-		rect3.y = -10;
-		count++;
-		countText.text = count;
-	}
-	if(rect4.y>500) {
-		rect4.y = -10;
-		count++;
-		countText.text = count;
-	}
-});
-drop.stop();
-
-function hitTestRectangle(r1, r2) {
-
-  //Define the variables we'll need to calculate
-  var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
-
-  //hit will determine whether there's a collision
-  hit = false;
-
-  //Find the center points of each sprite
-  r1.centerX = r1.x + r1.width / 2;
-  r1.centerY = r1.y + r1.height / 2;
-  r2.centerX = r2.x + r2.width / 2;
-  r2.centerY = r2.y + r2.height / 2;
-
-  //Find the half-widths and half-heights of each sprite
-  r1.halfWidth = r1.width / 2;
-  r1.halfHeight = r1.height / 2;
-  r2.halfWidth = r2.width / 2;
-  r2.halfHeight = r2.height / 2;
-
-  //Calculate the distance vector between the sprites
-  vx = r1.centerX - r2.centerX;
-  vy = r1.centerY - r2.centerY;
-
-  //Figure out the combined half-widths and half-heights
-  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
-  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
-
-  //Check for a collision on the x axis
-  if (Math.abs(vx) < combinedHalfWidths) {
-
-    //A collision might be occuring. Check for a collision on the y axis
-    if (Math.abs(vy) < combinedHalfHeights) {
-
-      //There's definitely a collision happening
-      hit = true;
-    } else {
-
-      //There's no collision on the y axis
-      hit = false;
+    for (var i = 1; i <= n; i++) {
+        ar.push(i);
     }
-  } else {
 
-    //There's no collision on the x axis
-    hit = false;
-  }
+    for (var i = 0; i < ar.length; i++) {
+        rnum = Math.floor(Math.random() * n);
+        temp = ar[i];
+        ar[i] = ar[rnum];
+        ar[rnum] = temp;
+    }
 
-  //`hit` will be either `true` or `false`
-  return hit;
-};*/
+    return ar;
+}
